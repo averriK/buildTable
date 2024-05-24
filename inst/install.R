6d34790d18b016de6f094629ee363524052b2851
Alejandro Verri <averri@gmail.com>
2024-05-24 17:28:55 -0300

    Updated author's email address
    The author's email address has been updated in both the DESCRIPTION and install.R files.

--- inst/install.R
+++ inst/install.R
@@ -3,7 +3,7 @@ file.remove("NAMESPACE")
 #   given="Alejandro",
 #   family = "Verri Kozlowski",
 #   role = c("aut", "cre"),
-#   email= "averri@fi.uba.ar",
+#   email= "averri@ni.uba.ar",
 #   comment = c(ORCID = "0000-0002-8535-1170")
 # )
 usethis::use_gpl_license(version = 3, include_future = TRUE)
